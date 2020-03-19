4a5469042e15451496913e8b320c9fc79e75cb55
Jackie Atwood <takalaj@verizon.net>
2020-03-18 22:26:23 -0400

    MVP met, attempting stretch goals

--- server.js
+++ server.js
@@ -1,13 +1,45 @@
 const express = require('express');
+const userRouter = require("./users/userRouter");
 
 const server = express();
+
+server.use(express.json());
+server.use(logger);
+// server.use(validateUserId);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.use("/api/users", userRouter);
+
 //custom middleware
 
-function logger(req, res, next) {}
+//`logger()`
+//`logger` logs to the console the following information about each request: request method, request url, and a timestamp
+//this middleware runs on every request made to the API
+function logger(req, res, next) {
+  console.log(req.method, req.url, Date.now())
+  next();
+}
+
+//`validateUserId()`
+//`validateUserId` validates the user id on every request that expects a user id parameter
+//if the `id` parameter is valid, store that user object as `req.user`
+//if the `id` parameter does not match any user id in the database, cancel the request and respond with status `400` and `{ message: "invalid user id" }`
+function validateUserId(req, res, next) {}
+
+//`validateUser()`
+//`validateUser` validates the `body` on a request to create a new user
+//if the request `body` is missing, cancel the request and respond with status `400` and `{ message: "missing user data" }`
+//if the request `body` is missing the required `name` field, cancel the request and respond with status `400` and `{ message: "missing required name field" }`
+function validateUser(req, res, next) {}
+
+//`validatePost()`
+//`validatePost` validates the `body` on a request to create a new post
+//if the request `body` is missing, cancel the request and respond with status `400` and `{ message: "missing post data" }`
+//if the request `body` is missing the required `text` field, cancel the request and respond with status `400` and `{ message: "missing required text field" }`
+function validatePost(req, res, next) {}
+
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,149 @@
 const express = require('express');
+const User = require("./userDb");
+const Post = require("../posts/postDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+
+//insert(): calling insert passing it a resource object will add it to the database and return the new resource.
+router.post('/', validateUser, (req, res) => {
+  const user = req.body;
+
+  User.insert(user)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "Could not add this user, sorry" });
+    })
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
-});
 
+//insert(): calling insert passing it a resource object will add it to the database and return the new resource.
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+  const newPost = req.body;
+
+    Post.insert(newPost)
+      .then(post => {
+        res.status(201).json(post);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ errorMessage: "No Post for you" })
+      })
+    });
+
+
+// get(): calling find returns a promise that resolves to an array of all the resources contained in the database.
 router.get('/', (req, res) => {
-  // do your magic!
+  User.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "500/catch for the GET users call" });
+    })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+
+// getById(): takes an id as the argument and returns a promise that resolves to the resource with that id if found.
+router.get('/:id', validateUserId, (req, res) => {
+  res.status(200).json(req.user);
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+
+//getById(): takes an id as the argument and returns a promise that resolves to the resource with that id if found.
+router.get('/:id/posts', validateUserId, (req, res) => {
+  const {id} = req.params;
+
+  User.getUserPosts(id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: `ID #${req.params.id} does not have any comments` });
+    })
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+
+//remove(): the remove method accepts an id as it's first parameter and, upon successfully deleting the resource from the database, returns the number of records deleted.
+router.delete('/:id', validateUserId, (req, res) => {
+  User.remove(req.params.id)
+    .then(() => {
+        return res.status(200).json({ message: `Bye Felicia - ID #${req.params.id} is out`})
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: `You are stuck with ID #${id}, deal with it` });
+    })
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
-});
+
+//update(): accepts two arguments, the first is the id of the resource to update and the second is an object with the changes to apply. It returns the count of updated records. If the count is 1 it means the record was updated correctly.
+router.put('/:id', validateUserId, (req, res) => {
+  const {id} = req.params;
+  const userUpdate = req.body;
+
+  User.update(id, userUpdate)
+    .then(user => {
+      if(user) {
+        User.getById(id)
+          .then(user => {
+            res.status(200).json(user);
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json({ errorMessage: "Could not get user" });
+          })
+      }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ errorMessage: "Could not update this user" })
+      })
+  });
+
+
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const {id} = req.params;
+
+  User.getById(id)
+  .then(user => {
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(404).json({ errorMessage: `User with this ID # ${req.params.id} does not exist` })
+    }
+  })
 }
 
+
 function validateUser(req, res, next) {
-  // do your magic!
+  const {name} = req.body;
+
+  if (!name) {
+    return res.status(400).json({ errorMessage: "Cannot be empty" });
+  }
+  req.body = { name };
+  next();
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+ const {id: user_id} = req.params;
+ const {text} = req.body;
+
+ if (!req.body || !{text}) {
+   return res.status(400).json({errorMessage: "Cannot be empty" });
+ }
+ req.body = {user_id, text};
+ next();
 }
 
 module.exports = router;
